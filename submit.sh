978c4959a2195e57d24e21f2b8e302e75e2f0e10
Ido Goldberg <gogoi@cs.huji.ac.il>
2020-08-19 12:40:28 +0300

    sumbit script

--- submit.sh
+++ submit.sh
@@ -9,5 +9,5 @@
 #ask for 2g for the job
 #SBATCH --mem=2g
 #SBATCH --mail-user=ido.goldb@gmail.com
-source
-python helloWorld.R
+source /ems/elsc-labs/loewenstein-y/ido.goldberg/WANN/brain-tokyo-workshop/venv/bin/activate
+python /ems/elsc-labs/loewenstein-y/ido.goldberg/WANN/brain-tokyo-workshop/WANNRRelease/WANN/wann_train.py -p /ems/elsc-labs/loewenstein-y/ido.goldberg/WANN/brain-tokyo-workshop/WANNRRelease/WAN/p/improved_wan.json
