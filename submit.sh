5746297ba1f6bdb2299a4c24724fb8b2ffa1823a
Ido Goldberg <gogoi@cs.huji.ac.il>
2020-08-20 15:03:52 +0300

    added requirements

--- submit.sh
+++ submit.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#Write output to helloWorld-JOB_ID.out
+#Write output to WANN-STDOUT-JOB_ID.out
 #SBATCH -J WANN
-#SBATCH -o helloWorl-%j.out
-#Write error output to helloWorl-JOB_ID.err
-#SBATCH -e helloWorl-%j.err
+#SBATCH -o WANN-STDOUT-%j.out
+#Write error output to WANN-STDERR-JOB_ID.err
+#SBATCH -e WANN-STDERR-%j.err
 #submit to elsc.q partition
 #SBATCH --partition=elsc.q
 #ask for 2g for the job
 #SBATCH --mem=2g
 #SBATCH --mail-user=ido.goldb@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH -c 9
-#SBATCH --ntasks=9
+#SBATCH -cpus-per-task 1
+#SBATCH --ntasks=33
 
 source /ems/elsc-labs/loewenstein-y/ido.goldberg/WANN/brain-tokyo-workshop/venv/bin/activate
-python WANNRelease/WANN/wann_train.py -p WANNRelease/WANN/p/default_wan.json -d WANNRelease/WANN/p/default_wan.json
+python WANNRelease/WANN/wann_train.py -p WANNRelease/WANN/p/default_wan.json -d WANNRelease/WANN/p/default_wan.json -n 32
