d0d801b76dc1a78790ca6d62709cc7b501ecc4fb
Ido Goldberg <gogoi@cs.huji.ac.il>
2020-08-25 16:03:35 +0300

    submit changes

--- submit.sh
+++ submit.sh
@@ -9,7 +9,7 @@
 #SBATCH --partition=elsc.q
 #SBATCH --mail-user=ido.goldb@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH -cpus-per-task=1
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks=33
 
 source /ems/elsc-labs/loewenstein-y/ido.goldberg/WANN/brain-tokyo-workshop/venv/bin/activate
