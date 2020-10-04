4c37a1333e9ff00f849af613bbff7ea9d703a8f9
Ido Goldberg <gogoi@cs.huji.ac.il>
2020-10-04 11:08:11 +0300

    changed submit

--- submit.sh
+++ submit.sh
@@ -2,9 +2,9 @@
 
 #Write output to biped_STDOUT_JOB_ID.out
 #SBATCH -J WANN
-#SBATCH -o biped_new_act_STDOUT_%j.out
-#Write error output to biped_STDERR_JOB_ID.err
-#SBATCH -e biped_STDERR_%j.err
+#SBATCH -o swing_RNN_STDOUT_%j.out
+#Write error output to swing_RNN_STDERR_JOB_ID.err
+#SBATCH -e swing_RNN_STDERR_%j.err
 #submit to elsc.q partition
 #SBATCH --partition=elsc.q
 #SBATCH --mail-user=ido.goldb@gmail.com
@@ -13,4 +13,4 @@
 #SBATCH --ntasks=33
 
 source /ems/elsc-labs/loewenstein-y/ido.goldberg/WANN/brain-tokyo-workshop/venv/bin/activate
-python WANNRelease/WANN/wann_train.py -p WANNRelease/WANN/p/biped.json -d WANNRelease/WANN/p/default_wan.json -n 32
+python WANNRelease/WANN/wann_train.py -n 32
